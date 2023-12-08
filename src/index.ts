105216762df4af94414c2b6f2f946fafb8ca9d0e
woo3145 <lcwoo3145@gmail.com>
2023-12-08 23:40:09 +0900

    고래변신기 0.1v

--- src/index.ts
+++ src/index.ts
@@ -3,45 +3,12 @@
  */
 
 import { ScriptPlayer } from 'zep-script';
-import { add } from './utils/add';
-
-const openWidget = (player: ScriptPlayer) => {
-  if (player.isMobile) {
-    player.tag.widget = player.showWidget('widget.html', 'sidebar', 300, 500);
-  } else {
-    player.tag.widget = player.showWidget('widget.html', 'sidebar', 290, 460);
-  }
-
-  // send message to widget
-  player.tag.widget.sendMessage({
-    isMobile: player.isMobile,
-  });
-
-  // handle message from child frame(widget)
-  player.tag.widget.onMessage.Add((sender: ScriptPlayer, msg: any) => {
-    switch (msg.type) {
-      case 'close':
-        if (sender.tag.widget) {
-          sender.tag.widget.destroy();
-          sender.tag.widget = null;
-        }
-        break;
-      case 'submit':
-        sender.showAlert('OK', (res: boolean) => {
-          return {};
-        });
-        break;
-    }
-  });
-};
-
-let zepLogo = ScriptApp.loadSpritesheet('whale.png');
 
 // 변수에 SpriteSheet를 읽어 저장
 const whale = ScriptApp.loadSpritesheet(
-  'image.png',
-  100,
-  100,
+  'whale_sprite.png',
+  48,
+  64,
   {
     left: [0], // left 라는 이미 정해진 왼쪽 방향으로 걸을 때의 애니메이션 이름
     up: [0], // 그 이름에 쓰일 전체 파일에서의 인덱스 넘버들
@@ -51,16 +18,49 @@ const whale = ScriptApp.loadSpritesheet(
   1
 ); // 1초에 8장으로 한다.
 
-// 플레이어가 입장할 때 바로 고래 그림으로 교체해준다.
-ScriptApp.onJoinPlayer.Add(function (player) {
-  player.sprite = whale;
+function openWidget(p: ScriptPlayer) {
+  if (p.isMobile)
+    //Set the widget's name, anchor position, width, and height.
+    p.tag.widget = p.showWidget('widget.html', 'sidebar', 300, 550);
+  else p.tag.widget = p.showWidget('widget.html', 'sidebar', 280, 520);
 
-  // 플레이어 속성이 변경되었으므로 호출해서 실제 반영해준다.
-  player.sendUpdated();
-});
+  //Send data to widget
+  p.tag.widget.sendMessage({
+    isMobile: p.isMobile,
+  });
 
-ScriptMap.putObject(0, 0, zepLogo, { overlap: true });
+  //Register events for messages sent from widgets
+  p.tag.widget.onMessage.Add(function (sender: any, msg: any) {
+    switch (msg.type) {
+      case 'close':
+        if (p.tag.widget) {
+          p.tag.widget.destroy();
+          p.tag.widget = null;
+        }
+
+        break;
+      case 'submit':
+        let choiceItem = msg.choice;
+        if (choiceItem == 2) {
+          sender.sprite = whale;
+        } else if (choiceItem == 1) {
+          sender.sprite = null;
+        }
+        sender.sendUpdated();
+        break;
+    }
+  });
+}
 
 ScriptApp.onDestroy.Add(function () {
   ScriptMap.clearAllObjects();
+});
+
+ScriptApp.onJoinPlayer.Add(function (p) {
+  //Reset the player's tag value
+  p.tag = {};
+});
+// Action when the player clicks the sidebar app
+ScriptApp.onSidebarTouched.Add(function (p) {
+  openWidget(p);
 });
